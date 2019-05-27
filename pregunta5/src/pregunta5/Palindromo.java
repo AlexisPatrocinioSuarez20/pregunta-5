8ae981befb13de6bd957a13b152c9668c5d981f9
ALEXIS <ALEXIS@DESKTOP-OFFQSQ7>
2019-05-26 22:56:20 -0500

    Determinar si una palabra o frase es un palindromo

---
+++ pregunta5/src/pregunta5/Palindromo.java
@@ -0,0 +1,26 @@
+package pregunta5;
+import java.util.Scanner;
+public class Palindromo {
+	
+	Scanner entrada = new Scanner(System.in);
+	String palabra="",nuevo="";
+	int j;
+	System.out.println("Ingrese la palabra deseada");
+
+	palabra=entrada.nextLine();
+	
+	j=palabra.length();
+	char al[]=new char[j];
+	for(int i=0;i<j;i++) {
+		al[j-i-1]=palabra.charAt(i);
+	}
+	nuevo=String.valueOf(al);
+	if(nuevo.equalsIgnoreCase(palabra)) {
+		System.out.print("Es palíndromo");
+		}
+	else {
+		System.out.print("No es palíndromo");
+	}
+}
+}
+
